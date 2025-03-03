acda80240b6931c820991f4d34d284e0b0483223
Ba1igel <b_chinaliyev@kbtu.kz>
2025-03-03 09:35:48 +0500

    lab.py

--- lab5/5.py
+++ lab5/5.py
@@ -1,7 +1,7 @@
 #Write a Python program to find the sequences of one upper case letter followed by lower case letters.
 
 import re
-pattern = r"[a-z]+_"
+pattern = r"[A-Z][a-z]"
 def find_pattern(s):
     return bool(re.search(pattern,s))
 texts = ['a', 'ab', 'AAA_b','aab_acbc', 'va', 'ca', 'ababU', 'abbbbbb', 'baaaaaJ', 'baGbanbsbaban']
